65d7e24f429a741dcadbd44985981f5241c7f43c
Daijiro Sagane <daijirosagane@DaijironoMacBook-Pro.local>
2024-01-13 18:56:19 +0900

    I finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,6 +6,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Random random = new Random();
+        int m = random.Next(1, 100);
+        int count = 0;
+
+        while (true) 
+        {
+            Console.Write("What is your guess?  ");
+            string gString = Console.ReadLine();
+            int g = int.Parse(gString);
+            count += 1;
+            
+            if (m < g) 
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (m > g)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (m == g)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"number of attempts:  {count}");
+                Console.Write("Do you want to play again? (yes/no):  ");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    m = random.Next(1, 100);
+                    count = 0;
+                }
+                else if (answer == "no")
+                {
+                    break;
+                }
+            }
+        }
     }
 }
