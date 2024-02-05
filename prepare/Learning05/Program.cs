1fb1b21eb593be1707d14624c8ab0c9857aca9d8
Daijir <dazhilangxianggen@gmail.com>
2024-02-05 12:34:02 +0900

    I finished Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("red", 7);
+        shapes.Add(square);
+        Rectangle rectangle = new Rectangle("blue", 5, 4);
+        shapes.Add(rectangle);
+        Circle circle = new Circle("pink", 5);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
