92be4863309fd3dfd8216ecbd67353ed9665fac5
Daijir <dazhilangxianggen@gmail.com>
2024-02-10 19:36:33 +0900

    I added comment for exceeding requirement

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
         static void Main(string[] args)
     {
+        // Showing Creativity and Exceeding Requirements:
+        // I added level up function in GoalManafer class.
         GoalManager gm = new GoalManager();
         gm.Start();
     }
